2a9bd1cfce2d2ebe524329a9683ed3a60122f513
Максим Герасименко <76556732+TolyanMAN@users.noreply.github.com>
2021-12-11 16:01:11 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,37 +1,32 @@
 /*let object = {};
-
 function A()
 {
-    return object;
+return object;
 }
 function B()
 {
-    return object;
+ return object;
 }
 alert( new A() == new B() )
 */
-
-
-// -------------------Второе задание-------------------
+//Второе задание
 
 /*function Fcalculator() {
 
-    this.read = function() {
-      this.a = +prompt('first element');
-      this.b = +prompt('second element');
+ this.read = function() {
+ this.a = +prompt('first element');
+   this.b = +prompt('second element');
     };
-  
     this.sum = function()
     {
-      return this.a + this.b;
+   return this.a + this.b;
     };
   
     this.mul = function()
     {
-      return this.a * this.b;
+   return this.a * this.b;
     };
   }
-  
   let calculator = new Fcalculator();
   calculator.read();
   
@@ -39,17 +34,15 @@ alert( new A() == new B() )
   alert("Mul = " + calculator.mul());
   */
 
-// -------------------Третье задание-------------------
+//Третье задание
 
-  function FConstructor(firstValue) {
+function FConstructor(firstValue) {
     this.value = firstValue;
     this.read = function()
     {
-      this.value += +prompt('Сколько добавить?');
+   this.value += +prompt('Сколько добавить?');
     };
-  
   }
-  
   let Constructor = new FConstructor(1);
   Constructor.read();
   Constructor.read();
